c00a65e0264dd00dcfee8867f1073b7266750b5f
Zephyr Mei <zephyr.mei@oocl.com>
2019-07-19 14:52:47 +0800

    myCenter

--- src/myCenter/index.vue
+++ src/myCenter/index.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
         <Header title="个人中心"></Header>
-        <p>暂未开发</p>
+        <a-card hoverablestyle="width: 300px">
+  <a-card-meta
+    :title="userInfo.nickname"
+    :description="userInfo.description">
+    <a-avatar slot="avatar" src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+  </a-card-meta>
+</a-card>
         <Navbar/>
     </div>
 </template>
@@ -9,11 +15,45 @@
 <script>
 import Header from '../components/header.vue';
 import Navbar from '../components/navbar.vue';
+import axios from "axios";
 
 export default {
   components: {
     Header,
     Navbar,
   },
+  data(){
+    return{
+      userInfo:{
+        nickname:"nickname",
+        avator:"",
+        description:"This is my description.",
+      }
+      
+    }
+  },
+  methods:{
+    getUserInfo(){
+      axios({
+        method:"get",
+        url:`/users/test`,
+        timeout: 10000
+      })
+      .then(res=>{
+        this.userInfo.nickname=res.data.nickname;
+        this.userInfo.description=res.data.introduction;
+        console.log(res.data.nickname,res.data.introduction);
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    }
+  },
+  created(){
+    this.getUserInfo();
+  }
 };
 </script>
+
+<style>
+</style>
